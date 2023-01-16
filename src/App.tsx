edeb13274ea7290d3b6f84c1c4fbb887b0dd7d82
Justin Behnke <justin.behnke@spg-tech.com>
2023-01-16 16:26:37 -0600

    Add the interviews

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,19 @@ import people from "./data/people.json";
 import Person from "./Person";
 import ListGroup from "react-bootstrap/ListGroup";
 
+const interviews = [
+  {
+    date: "7 Jan 2023",
+    interviewee: "Robert Behnke",
+    downloadable: "media/BobBehnke-PhoneInterview-Jan7th2023.txt",
+  },
+  {
+    date: "10 Jan 2023",
+    interviewee: "Delaine Richards",
+    downloadable: "media/DelaineDorisRichards-Interview10Jan2023.pdf",
+  },
+];
+
 function App(): ReactElement {
   const [userId, setUser] = useState<number>();
 
@@ -12,18 +25,33 @@ function App(): ReactElement {
     <Container className="App">
       {!!userId && <Person userId={userId} clearPersonId={() => setUser(0)} />}
       {!userId && (
-        <ListGroup>
-          {people.map((person) => {
-            return (
-              <ListGroup.Item
-                key={person.id}
-                onClick={() => setUser(person.id)}
-              >
-                {person.name}
-              </ListGroup.Item>
-            );
-          })}
-        </ListGroup>
+        <div>
+          <h3>People Pages</h3>
+          <ListGroup>
+            {people.map((person) => {
+              return (
+                <ListGroup.Item
+                  key={person.id}
+                  onClick={() => setUser(person.id)}
+                >
+                  {person.name}
+                </ListGroup.Item>
+              );
+            })}
+          </ListGroup>
+          <h3>Interviews</h3>
+          <ListGroup>
+            {interviews.map((interview) => {
+              return (
+                <ListGroup.Item key={interview.interviewee + interview.date}>
+                  <a href={interview.downloadable} download>
+                    {interview.interviewee + " " + interview.date}
+                  </a>
+                </ListGroup.Item>
+              );
+            })}
+          </ListGroup>
+        </div>
       )}
     </Container>
   );
